f07207795a63adea3dee1dc00b889925605b2602
aaaa3521 <24bn023f@rikkyo.ac.jp>
2026-04-10 14:25:13 +0900

    マップ: 国土地理院タイル（日本語）に変更

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -174,8 +174,8 @@ export default function MapView() {
           zoomControl={false}
         >
           <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
           />
 
           {/* 全ルートの点線 */}
