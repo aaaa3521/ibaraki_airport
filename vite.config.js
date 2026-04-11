6cad55239b75227ecf65d29c48ab8c1effba4977
aaaa3521 <24bn023f@rikkyo.ac.jp>
2026-04-11 13:05:31 +0900

    vite: Vercelは/、GitHub Pagesは/ibaraki_airport/に切り替え

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ibaraki_airport/',
+  base: process.env.VERCEL ? '/' : '/ibaraki_airport/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
