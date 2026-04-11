5ff45a7d6912dca30821f10b1456d9b9d56fd0e0
aaaa3521 <24bn023f@rikkyo.ac.jp>
2026-04-11 14:18:22 +0900

    マップ: OSM Japan日本語タイルに変更

--- src/pages/MapView.jsx
+++ src/pages/MapView.jsx
@@ -170,16 +170,10 @@ export default function MapView() {
           style={{ height: '100%', width: '100%', position: 'absolute', inset: 0 }}
           zoomControl={false}
         >
-          {/* 白ベース下地 */}
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          />
-          {/* 日本語ラベルのみ重ね（国土地理院） */}
-          <TileLayer
-            url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
-            opacity={0.6}
+            url="https://tile.openstreetmap.jp/styles/osm-bright/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            className="map-tiles-muted"
           />
 
           {/* 各トラックのルートライン */}
